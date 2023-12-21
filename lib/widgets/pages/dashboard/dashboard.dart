68585e054e42b7eb502258443a5167678bffc45b
Yusuf Rizal <yusufrizalh@gmail.com>
2023-12-21 14:35:36 +0700

    flutter get data and search

--- lib/widgets/pages/dashboard/dashboard.dart
+++ lib/widgets/pages/dashboard/dashboard.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app/main.dart';
+import 'package:flutter_app/widgets/pages/dashboard/productlist.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Dashboard extends StatefulWidget {
@@ -12,6 +13,28 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  void handleSelectedMenuButton(String menu) {
+    switch (menu) {
+      case "Products":
+        debugPrint("Products");
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ProductList(),
+          ),
+        );
+        break;
+      case "Logout":
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Home(),
+          ),
+        );
+        break;
+    }
+  }
+
   userLogout() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setBool("login", false);
@@ -28,9 +51,16 @@ class _DashboardState extends State<Dashboard> {
         centerTitle: true,
         automaticallyImplyLeading: false,
         actions: <Widget>[
-          IconButton(
-            onPressed: () => userLogout(),
-            icon: const Icon(Icons.logout),
+          PopupMenuButton(
+            onSelected: handleSelectedMenuButton,
+            itemBuilder: (context) {
+              return {"Products", "Logout"}.map((String menu) {
+                return PopupMenuItem(
+                  value: menu,
+                  child: Text(menu),
+                );
+              }).toList();
+            },
           ),
         ],
       ),
---
+++ lib/widgets/pages/dashboard/productlist.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import '../dashboard/models/productmodel.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  final String apiUrl =
+      "http://192.168.1.6/flutter-api/products/getAllProducts.php";
+
+  List<dynamic> productList = []; // nilai awal masih kosong
+  final productModel = ProductModel();
+
+  getProductList() async {
+    try {
+      var resp = await http.get(Uri.parse(apiUrl));
+      if (resp.statusCode == 200) {
+        var response = convert.json.decode(resp.body);
+        if (response["status"] == "OK") {
+          debugPrint("RESPONSE: ${resp.body}");
+          List<dynamic> products = response["data"];
+          return products;
+        }
+      } else {
+        throw Exception("Error while getting all products");
+      }
+    } catch (e) {
+      debugPrint("Error: $e");
+    }
+  }
+
+  List<dynamic> productSearch = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getProductList().then((data) {
+      setState(() {
+        productSearch = data;
+        productList = productSearch;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Product List"),
+        centerTitle: true,
+      ),
+      body: RefreshIndicator(
+        strokeWidth: 3,
+        onRefresh: () {
+          return Future.delayed(
+            const Duration(seconds: 3),
+            () {
+              setState(() {
+                getProductList().then((data) {
+                  setState(() {
+                    productList = data;
+                  });
+                });
+              });
+              final mySnackbar = SnackBar(
+                content: const Text("Success to refresh data"),
+                backgroundColor: Colors.orange,
+                elevation: 4,
+                margin: const EdgeInsets.all(12),
+                behavior: SnackBarBehavior.floating,
+                showCloseIcon: true,
+                closeIconColor: Colors.white,
+                dismissDirection: DismissDirection.down,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                duration: const Duration(seconds: 3),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(mySnackbar);
+            },
+          );
+        },
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(12),
+                child: TextField(
+                  textInputAction: TextInputAction.search,
+                  decoration: const InputDecoration(
+                    hintText: "Search product name",
+                    suffixIcon: Icon(Icons.search),
+                  ),
+                  onChanged: (keyword) {
+                    setState(() {
+                      productList = productSearch
+                          .where((productFound) => productFound["name"]
+                              .toLowerCase()
+                              .contains(keyword.toLowerCase()))
+                          .toList();
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(4),
+                  itemCount: productList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.all(8),
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey.shade200),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(
+                        leading: const Icon(Icons.numbers),
+                        title: Text(productList[index]["name"].toString()),
+                        subtitle: Text(productList[index]["price"].toString()),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
